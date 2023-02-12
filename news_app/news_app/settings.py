9c480e70bdeb4935fbe9705b1ad8c1d6547d574a
Diana-Verevkina <diva2208@mail.ru>
2023-02-13 00:47:42 +0300

    redoc,swagger

--- news_app/news_app/settings.py
+++ news_app/news_app/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
-    'django_extensions'
+    'django_extensions',
+    'drf_yasg',
     # 'djoser'
 
 ]
